d7abcc9af70253f82987a5e1916a1adedadfdedc
Vaclav Klecanda <vencax77@gmail.com>
2014-11-08 20:42:58 +0100

    loading screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,20 +6,39 @@ angular.module('starter.controllers', [])
   });
 })
 
-.controller('BuyCtrl', function($scope, $rootScope, $location, TicketSrvc) {
+.controller('BuyCtrl', function($scope, $rootScope, $location, $ionicLoading, TicketSrvc) {
+  $scope.show = function() {
+    $ionicLoading.show({
+      template: 'Loading...'
+    });
+  };
+  $scope.hide = function(){
+    $ionicLoading.hide();
+  };
+
   $scope.data = [];
-  TicketSrvc.list().success(function(data) {
+  $scope.show();
+
+  TicketSrvc.list()
+  .success(function(data) {
     $scope.data = data;
+    $scope.hide();
+  })
+  .error(function(err, status){
+    $scope.hide();
   });
 
   $scope.buy = function(ticket) {
+    $scope.show();
     TicketSrvc.buy(ticket).success(function(data) {
       $rootScope.loggedUser.credit -= ticket.amount;
       $location.path('/');
+      $ionicLoading.hide();
     }).error(function(err, status){
       if (status === 400) {
         alert('Not enough money!');
       }
+      $ionicLoading.hide();
     });
   };
 })
